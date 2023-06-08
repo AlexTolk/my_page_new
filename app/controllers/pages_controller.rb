adacce0c75135fe50e038990e3a2e9c0050c6b4c
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2023-06-08 17:07:33 +0800

    feat: about page, projects page, stack page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def index
   end
+  def stack
+  end
+  def about
+  end
+  def projects
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'pages#index'
+  root "pages#index"
+  get "projects", to: "pages#projects"
+  get "about", to: "pages#about"
+  get "stack", to: "pages#stack"
 end
