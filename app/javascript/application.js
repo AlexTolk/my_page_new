a0f6e331500553eb24545d9ccb58ee3167c8f5a1
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2023-08-03 13:53:46 +0800

    used index.js instead of application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,16 +2,16 @@
 import "@hotwired/turbo-rails";
 import "./components/index.js";
 
-function rotate() {
-  var lastChild = document.querySelector('.slider div:last-child').cloneNode()
+// function rotate() {
+//   var lastChild = document.querySelector('.slider div:last-child').cloneNode()
 
-  /*$('#test').html(lastChild)*/
-  document.querySelector('.slider div').classList.remove('firstSlide')
-  document.querySelector('.slider div:last-child').remove();
-  document.querySelector('.slider').prepend(lastChild)
-  document.querySelector(lastChild).addClass('firstSlide')
-}
+//   /*$('#test').html(lastChild)*/
+//   document.querySelector('.slider div').classList.remove('firstSlide')
+//   document.querySelector('.slider div:last-child').remove();
+//   document.querySelector('.slider').prepend(lastChild)
+//   document.querySelector(lastChild).addClass('firstSlide')
+// }
 
-window.setInterval(function(){
-  rotate()
-}, 4000);
+// window.setInterval(function(){
+//   rotate()
+// }, 4000);
