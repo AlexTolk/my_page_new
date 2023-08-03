f83a66c6333113669ae1f8f2322e2031fcac3456
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2023-08-03 19:08:41 +0800

    edited index page: projects background, carousel in stack

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "./components/index.js";
-
-// function rotate() {
-//   var lastChild = document.querySelector('.slider div:last-child').cloneNode()
-
-//   /*$('#test').html(lastChild)*/
-//   document.querySelector('.slider div').classList.remove('firstSlide')
-//   document.querySelector('.slider div:last-child').remove();
-//   document.querySelector('.slider').prepend(lastChild)
-//   document.querySelector(lastChild).addClass('firstSlide')
-// }
-
-// window.setInterval(function(){
-//   rotate()
-// }, 4000);
--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,13 +1,1 @@
-function rotate() {
-  var lastChild = document.querySelector('.slider div:last-child').cloneNode()
-
-  /*$('#test').html(lastChild)*/
-  document.querySelector('.slider div').classList.remove('firstSlide')
-  document.querySelector('.slider div:last-child').remove();
-  document.querySelector('.slider').prepend(lastChild)
-  document.querySelector(lastChild).addClass('firstSlide')
-}
-
-window.setInterval(function(){
-  rotate()
-}, 4000);
+console.log("Hello from index.js")
