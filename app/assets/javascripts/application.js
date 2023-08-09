9bf03991951948a45724fdb5df379b58161182f9
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2023-08-09 21:10:01 +0800

    bundle installed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import './components/index.js'
-console.log('welcome from application.js')
+console.log('hello from application.js')
